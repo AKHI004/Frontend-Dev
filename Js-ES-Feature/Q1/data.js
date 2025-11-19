c9d5acbe6714ac255a775992e9b01f92133004bf
Akshat <akhi142004@gmail.com>
2025-11-19 10:18:46 +0530

    Assignment-18

--- Js-ES-Feature/Q1/data.js
+++
@@ -1,8 +0,0 @@
-// Mixed API data (hoisting note: const declarations are NOT hoisted like var)
-const apiData = ["25", "true", "false", "NaN", " ", "100px", "3.14", null, undefined];
-
-// Exporting for use in other files (if running in Node)
-// If in browser, ignore the export line.
-if (typeof module !== "undefined") {
-    module.exports = { apiData };
-}
